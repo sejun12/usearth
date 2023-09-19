3e9f167cc51a2e4de7e997dc4baed5ce2e459263
parkcheonhwi <cjsgnl1995@gmail.com>
2023-09-19 17:04:27 +0900

    9/19 작업현황

---
+++ src/main/resources/static/js/memberWithdrawal/memberWithdrawal.js
@@ -0,0 +1,19 @@
+const openModalBtn= document.getElementById("openModal");
+const modal=document.getElementById("quitModal");
+const closeModalBtn=document.getElementById("closeModal");
+
+function openModal(){
+    modal.style.display="block";
+}
+function closeModal(){
+    modal.style.display="none";
+}
+// 버튼 클릭시 이벤트
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+
+window.addEventListener("click",(e)=>{
+    if(e.target === modal){
+        closeModal();
+    }
+});
---
+++ src/main/resources/static/js/modifyMyPassword/modifyMyPassword.js
@@ -0,0 +1,22 @@
+function confirmPassword(){
+    const newPassword=document.getElementById('password').value;
+    const confirmPassword=document.getElementById('passwordConfirm').value;
+    const resultDiv=document.getElementById('result');
+    const passwordRegex=/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+
+
+    if (passwordRegex.test(newPassword)) {
+        if (newPassword === confirmPassword) {
+            resultDiv.innerHTML = '비밀번호가 일치하며 유효합니다.';
+            resultDiv.style.color = 'green';
+
+        }else{
+            resultDiv.innerHTML = '비밀번호가 일치하지 않습니다.';
+            resultDiv.style.color = 'red';
+        }
+    }else{
+        resultDiv.innerHTML = '비밀번호는 최소 8자에서 16자까지, 영문자, 숫자 및 특수 문자를 포함해야 합니다.';
+        resultDiv.style.color = 'red';
+    }
+
+}
