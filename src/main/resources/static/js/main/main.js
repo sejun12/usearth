a868d1a88cf8a821b375aa8e538bcda40c3c00ed
parkcheonhwi <cjsgnl1995@gmail.com>
2023-10-23 14:35:35 +0900

    10/23

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -676,4 +676,9 @@ function scales(index, factors) {
     forthCardImage[index].style.transform = `scale(${factors})`;
 }
 
-// 모바일 더보기 
+// 모바일 더보기
+const moBtn= document.querySelector(".mlobilePrimaryFull");
+moBtn.addEventListener("click", clickHandler);
+function clickHandler(){
+    window.location.href="http://localhost:10000/recycling-agent/recycling-agent";
+}
